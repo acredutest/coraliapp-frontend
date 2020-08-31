03b6da7abd11577eb87980980b56b571ef1bdb75
fernandinux <fernando.paca@gmail.com>
2020-08-31 15:24:18 -0500

    serviceworker ok  y  verificador

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Footer from "./footer";
-import { pdfjs } from `react-pdf`;
+import { pdfjs } from "react-pdf";
 import * as serviceWorker from "./serviceWorker";
 
 export default function Home() {
